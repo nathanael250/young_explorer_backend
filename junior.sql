6e00e4be7cf7c7d97e24df8643bf059b99b88c2e
NIYOGUSHIMWA Nathanael <nathanaelniyogushimwa@gmail.com>
2026-07-23 10:53:29 +0200

    push changes in teh sql fille

--- junior.sql
+++ junior.sql
@@ -7,6 +7,7 @@
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -16,6 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+-- testing mode
 
 --
 -- Database: `junior`
